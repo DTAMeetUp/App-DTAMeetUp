903810adc21467a482d2661c648e08af75fe7b22
El Thomas <thomas.folmer@outlook.fr>
2017-10-28 13:23:31 +0200

    First Commit Login - Google Account working

--- app/src/main/java/fr/dta/formtion/dta_meetup/MainActivity.java
+++ app/src/main/java/fr/dta/formtion/dta_meetup/MainActivity.java
@@ -1,13 +1,59 @@
 package fr.dta.formtion.dta_meetup;
 
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button disconnectButton;
+    private FirebaseAuth mAuth;
+    private GoogleApiClient mGoogleApiClient;
+    public Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Get firebase instance
+        mAuth = FirebaseAuth.getInstance();
+
+        disconnectButton = (Button) findViewById(R.id.disconnectButton);
+        disconnectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                    // Firebase sign out
+                    mAuth.signOut();
+                    // Google sign out
+                Intent intent = new Intent (MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+
+                }
+            });
+
+
+
+
+
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        Log.d("On start","le user est " + currentUser.getDisplayName());
     }
 }
