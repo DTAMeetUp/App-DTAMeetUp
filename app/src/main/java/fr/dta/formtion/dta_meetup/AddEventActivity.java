154dfc9f8210156fec2e8d611d137805b4a02852
Unknown <drazx974@gmail.com>
2017-11-09 01:26:53 +0100

    Update ImageView when Spinner change

--- app/src/main/java/fr/dta/formtion/dta_meetup/AddEventActivity.java
+++ app/src/main/java/fr/dta/formtion/dta_meetup/AddEventActivity.java
@@ -4,6 +4,8 @@ import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -17,14 +19,18 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
 import fr.dta.formtion.dta_meetup.database.Event;
@@ -36,10 +42,14 @@ public class AddEventActivity extends AppCompatActivity {
     EditText locationEditText;
     EditText descriptionEditText;
     Spinner categoriesSpinner;
-    String selectedCategory;
     Button dateTimeButton;
     Calendar dateTime;
     Toolbar toolbar;
+    ImageView categoryImageView;
+
+    String selectedCategory;
+    TypedArray iconsArray;
+    List<String> categoriesArray;
     boolean dateSet = false;
 
 
@@ -47,29 +57,44 @@ public class AddEventActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_event);
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        // Toolbar
+        toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
+        // Current Context
         context = AddEventActivity.this;
 
 
+        // Get Views
         eventTitleEditText = (EditText) findViewById(R.id.eventTitleEditText);
         locationEditText = (EditText) findViewById(R.id.locationEditText);
         descriptionEditText = (EditText) findViewById(R.id.descriptionEditText);
         categoriesSpinner = (Spinner) findViewById(R.id.categoriesSpinner);
+        categoryImageView = (ImageView) findViewById(R.id.categoryImageView);
         dateTimeButton = (Button) findViewById(R.id.datetimeButton);
         dateTime = Calendar.getInstance();
 
-        ArrayAdapter<CharSequence> serviceAdapter = ArrayAdapter.createFromResource(this,
-                R.array.categories, android.R.layout.simple_spinner_item);
+
+        // Categories Icons
+        this.iconsArray = getResources().obtainTypedArray(R.array.icons);
+
+        // Categories Spinner Handler
+        this.categoriesArray = Arrays.asList(getResources().getStringArray(R.array.categories));
+        ArrayAdapter<CharSequence> serviceAdapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.categories,
+                android.R.layout.simple_spinner_item);
         serviceAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         categoriesSpinner.setAdapter(serviceAdapter);
+        categoriesSpinner.setSelection(0); // Event (the default event category)
         categoriesSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 selectedCategory = parent.getItemAtPosition(position).toString();
+                updateCategoryThumb(position);
             }
 
             @Override
@@ -79,36 +104,45 @@ public class AddEventActivity extends AppCompatActivity {
         });
 
 
+        // Date Time ClickListener
         dateTimeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.d("DPDIALOG", "button clicked");
-
-                DatePickerDialog dpDialog = new DatePickerDialog(context, new DatePickerDialog.OnDateSetListener() {
-                    @Override
-                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-                        dateTime.set(Calendar.YEAR, year);
-                        dateTime.set(Calendar.MONTH, month);
-                        dateTime.set(Calendar.DAY_OF_MONTH, dayOfMonth);
-                        TimePickerDialog tmDialog = new TimePickerDialog(context, new TimePickerDialog.OnTimeSetListener() {
-                            @Override
-                            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-                                dateTime.set(Calendar.HOUR_OF_DAY, hourOfDay);
-                                dateTime.set(Calendar.MINUTE, minute);
-                                dateTimeButton.setText(dateTime.get(Calendar.DAY_OF_MONTH) + " " + dateTime.getDisplayName(Calendar.MONTH, Calendar.SHORT, Locale.FRENCH)
-                                + " " + dateTime.get(Calendar.YEAR) + " à " + hourOfDay + ":" + String.format("%02d", minute));
-
-                                dateSet = true;
-
-                            }
-                        }, 20, 0, true);
-                        tmDialog.show();
-                    }
-                }, 2017, 11, 15);
-                dpDialog.show();
-
+                displayDatePickerDialog();
             }
         });
+    }
+
+    private void updateCategoryThumb(int position) {
+        String item = this.categoriesArray.get(position);
+        int srcDrawable = this.getResources().getIdentifier("ic_"+item, "drawable", MeetUp.getContext().getPackageName());
+        categoryImageView.setImageResource(srcDrawable);
+    }
+
+    private void displayDatePickerDialog() {
+        DatePickerDialog dpDialog = new DatePickerDialog(context, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                dateTime.set(Calendar.YEAR, year);
+                dateTime.set(Calendar.MONTH, month);
+                dateTime.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                TimePickerDialog tmDialog = new TimePickerDialog(context, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        dateTime.set(Calendar.HOUR_OF_DAY, hourOfDay);
+                        dateTime.set(Calendar.MINUTE, minute);
+                        dateTimeButton.setText(dateTime.get(Calendar.DAY_OF_MONTH) + " " + dateTime.getDisplayName(Calendar.MONTH, Calendar.SHORT, Locale.FRENCH)
+                        + " " + dateTime.get(Calendar.YEAR) + " à " + hourOfDay + ":" + String.format("%02d", minute));
+
+                        dateSet = true;
+
+                    }
+                }, 20, 0, true);
+                tmDialog.show();
+            }
+        }, 2017, 11, 15);
+        dpDialog.show();
     }
 
     protected boolean isEverythingFilled() {
